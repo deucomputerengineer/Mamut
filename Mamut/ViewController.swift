14e32467d1ea16d49c7887e59d2e1ef168b021fb
Tuğçe Dülge <dulgeetugce@gmail.com>
2019-01-23 19:47:36 +0300

    deneme
    first conflict

--- Mamut/ViewController.swift
+++ Mamut/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("hello girls")
+        print("hello girls!!!")
         print("Mine deneme")
     }
 
