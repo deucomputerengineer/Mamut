d62dbf59a1fc64c3f9294879fe554f80b88b0366
Mine <mineoz621@gmail.com>
2019-01-23 19:39:59 +0300

    Mine First Commit

--- Mamut/ViewController.swift
+++ Mamut/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("hello girls")
+        print("Mine deneme")
     }
 
 
